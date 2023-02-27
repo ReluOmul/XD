952d07f15cfba975e1ae5175f2c781c071b81171
ReluOmul <serbanradu2006@yahoo.com>
2023-02-27 21:26:44 +0200

    Determinarea primalitatii unui numar

---
+++ Numar.Prim/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Gray;
+        }
+
+        private void submitBTN_Click(object sender, EventArgs e)
+        {
+            int number;
+            bool isNumber = int.TryParse(numberTextBox.Text, out number);
+
+            if (!isNumber)
+            {
+                this.BackColor = Color.Yellow;
+                MessageBox.Show("Ati introdus un text, nu un numar");
+                numberTextBox.Text = "";
+            }
+            else
+            {
+                if (IsPrime(number))
+                {
+                    this.BackColor = Color.Green;
+                }
+                else
+                {
+                    this.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
